0a31cdda4434f938f959bd8b3ad5d6dd17c46f39
Pranav <pranav1813.cse18@chitkara.edu.in>
2022-03-09 17:09:12 +0530

    News-Completed

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -10,7 +10,7 @@ const Cryptocurrencies = ({ simplified }) => {
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
-  console.log(cryptos)
+  
 
   useEffect(() => {
     const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -16,9 +16,9 @@ export const cryptoNewsApi=createApi({
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints:(builder) =>({
         getCryptoNews:builder.query({
-            query:({newCategory, count})=> createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+            query:({newsCategory, count})=> createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
         })
     })
 })
 
-export const {useGetCryptoNewsQuery}= cryptoNewsApi;
+export const {useGetCryptoNewsQuery} = cryptoNewsApi;
