fb73ddace4b4ccca73598c3f4e3e5ab38913ab40
Pranav <pranav1813.cse18@chitkara.edu.in>
2022-03-11 14:08:45 +0530

    Crypto Details

--- src/components/Cryptodetails.jsx
+++ src/components/Cryptodetails.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import HTMLReactParser from 'html-react-parser'
+import {useParams} from 'react-router-dom'
+import millify from 'millify'
+import {Col, Row, Typography, Select} from 'antd'
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined,
+   StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import {useGetCryptoDetailsQuery} from '../services/cryptoapi'
 
-const Cryptodetsils = () => {
+const {Title,Text} = Typography
+const {Option} = Select
+
+
+const Cryptodetails = () => {
+  const {coinId}=useParams();
+  const [timePeriod, settimePeriod]=useState('7d')
+  const {data: A, isFetching} = useGetCryptoDetailsQuery(coinId)
+  console.log(A)
+
   return (
     <div>
-      Cryptodetails
+      Cryptodetails {coinId}
     </div>
   )
 }
 
-export default Cryptodetsils
+export default Cryptodetails
+ 
